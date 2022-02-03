7f4aade8411af49db27bcf7194563ad8fb3fb652
Prince28993 <gagandeepsingh28993@gmail.com>
2022-02-03 18:28:22 -0500

    First commit

---
+++ src/selenium/Testing3.java
@@ -0,0 +1,36 @@
+package selenium;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Testing3 {
+	SoftAssert softAssert = new SoftAssert();
+  @Test (priority = 1,groups = {"smoke","sanity"})
+  public void createUser() {
+	  System.out.println("I am in home test");
+	  System.out.println("Before assertion");
+	  Assert.assertTrue(4 > 3,"Verifying Element"); //we use regular assert in industrty
+	  System.out.println("After assertion");
+	  Assert.assertEquals("abc","abc");
+  }
+  @Test (priority = 2, dependsOnMethods = "createUser")//Edit user test@test.com
+  public void editUser() {
+	  
+	  System.out.println("Before assertion");
+	  softAssert.assertTrue(2 > 3,"Verifying Element"); 
+	  System.out.println("After assertion");
+	 softAssert.assertAll(); //Similar to error collectror in Junit
+}
+  
+  @Test (priority = 2, dependsOnMethods = "editUser")
+  public void deleteUser() {
+	  System.out.println("I am in end test");
+	  System.out.println("Before assertion");
+	  Assert.assertTrue(2 > 3,"Verifying Element"); 
+	  System.out.println("After assertion");
+	 softAssert.assertEquals("abc","abc1");
+	  System.out.println("After second assertion");
+	  softAssert.assertAll();
+  }
+}
